63825a419a8a28921fffc5b6b105ae78d3bb7b32
Bruno Araujo <brunoaraujo@hotmail.com>
2023-11-17 10:01:20 -0300

    Criando projeto para exemplo de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args){
+        String nomeCompleto = "Linguegem " + "Java";
+
+        System.out.println(nomeCompleto);
+
+    }
+}
--- tipos-variaveis/src/TipoisVariaveis.java
+++ tipos-variaveis/src/TipoisVariaveis.java
@@ -1,5 +1,23 @@
 public class TipoisVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //String que representa texto na aplicação
+
+        String meuNome = "Bruno Araújo";
+
+        int idade = 21;
+
+        short numeroCurto = 10;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(meuNome);
+
+        System.out.println(numeroNormal);
+
+        final double SALARIO_MINIMO = 2500;
+
+        System.out.println(SALARIO_MINIMO);
+        
     }
 }

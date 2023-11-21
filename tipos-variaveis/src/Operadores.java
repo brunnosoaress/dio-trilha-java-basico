4933c3ee9dd7207ff3fbda8f3ba3f2f0b19f7f68
Bruno Araujo <brunoaraujo@hotmail.com>
2023-11-21 11:41:06 -0300

    projeto basico de operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,13 +1,32 @@
 public class Operadores {
     public static void main(String[] args){
-        int a, b;
+        String nomeUm = "Bruno";
+        String nomeDois = new String("Bruno");
 
-        a = 10;
-        b = 12;
+        System.out.println(nomeUm.equals(nomeDois));
 
-        String resultado = a == b ? "verdadeiro" : "falso";
+        int numero1 = 1;
+        int numero2 = 2;
 
-        System.out.println(resultado);
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 < numero2){
+            System.out.println("A condição é verdadeira");
+        }
+        
+        System.out.println("numeroUm é igual numeroDois ? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de numeroDois ? " + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("numeroUm é menor que numeroDois ? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois ? " + simNao);
 
     }
 }

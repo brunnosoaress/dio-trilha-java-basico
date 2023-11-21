64b78056ec90173ba309af99c4d16698a434427a
Bruno Araujo <brunoaraujo@hotmail.com>
2023-11-21 11:54:50 -0300

    projeto basico de operadores logicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,32 +1,16 @@
 public class Operadores {
     public static void main(String[] args){
-        String nomeUm = "Bruno";
-        String nomeDois = new String("Bruno");
+        boolean condicao1 = true;
+        boolean condicao2 = false;
 
-        System.out.println(nomeUm.equals(nomeDois));
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if (numero1 < numero2){
-            System.out.println("A condição é verdadeira");
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
         }
-        
-        System.out.println("numeroUm é igual numeroDois ? " + simNao);
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das duas condições são verdadeira");
+        }
 
-        simNao = numero1 != numero2;
-
-        System.out.println("numeroUm é diferente de numeroDois ? " + simNao);
-
-        simNao = numero1 < numero2;
-
-        System.out.println("numeroUm é menor que numeroDois ? " + simNao);
-
-        simNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois ? " + simNao);
+        System.out.println("Fim");
 
     }
 }
